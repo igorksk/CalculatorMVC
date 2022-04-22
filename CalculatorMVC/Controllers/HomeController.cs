5039d0f7277da726fb48858c78fa79f4f9d7ec3f
Igor <ihor.kaseko@aploris.com>
2022-04-22 08:17:44 +0300

    Add project files.

---
+++ CalculatorMVC/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using CalculatorMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Operation model)
+        {
+            switch(model.OperationType)
+            {
+                case OperationType.Addition:
+                    model.Result = model.NumberA + model.NumberB;
+                    break;
+                case OperationType.Subtraction:
+                    model.Result = model.NumberA - model.NumberB;
+                    break;
+                case OperationType.Multiplication:
+                    model.Result = model.NumberA * model.NumberB;
+                    break;
+                case OperationType.Division:
+                    model.Result = model.NumberA / model.NumberB;
+                    break;
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CalculatorMVC/Models/Operation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorMVC.Models
+{
+    public class Operation
+    {
+        [Display(Name = "First Number")]
+        public double NumberA { get; set; }
+
+        [Display(Name = "Second Number")]
+        public double NumberB { get; set; }
+
+        [Display(Name = "Result")]
+        public double Result { get; set; }
+
+        [Display(Name = "Operation")]
+        public OperationType OperationType { get; set; }
+    }
+
+    public enum OperationType
+    {
+        Addition,
+        Multiplication,
+        Division,
+        Subtraction
+    }
+}
