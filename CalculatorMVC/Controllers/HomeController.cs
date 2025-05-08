c21fcec42e574fc040ee04b01baf90c9f4db4ceb
Igor Kaseko <igorksk@gmail.com>
2025-05-08 15:43:01 +0300

    remove logger

--- CalculatorMVC/Controllers/HomeController.cs
+++ CalculatorMVC/Controllers/HomeController.cs
@@ -1,19 +1,11 @@
 ﻿using CalculatorMVC.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
 namespace CalculatorMVC.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController() : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
         public IActionResult Index()
         {
